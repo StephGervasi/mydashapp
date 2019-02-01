370c611d3e20c544e043091ded5772eb66adf8bf
Stephanie Gervasi <steph.gervasi@gmail.com>
2019-02-01 17:23:06 -0500

    edits

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State, Event
+from dash.dependencies import Input, Output, State
 
 import pickle as pkl
 import numpy as np
